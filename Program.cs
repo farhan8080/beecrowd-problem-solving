d1886d15ea6602a63f68b4baa6d9d79bf292b3ba
farhan8080 <farhanshahriar802@gmail.com>
2025-07-30 20:07:42 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Remoting.Services;
+namespace ConsoleGame1
+  
+{
+   
+
+
+        class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            int km = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(km*2+ "minutos");
+
+
+
+
+        }
+    }
+}
